9731cc14ba8ad5df555bde779142413892fbe307
ambi hidalgo <ambio.pk@gmail.com>
2023-01-12 16:13:26 -0800

    Add getExchange async function, Addbasic UI Logic to print response, error and handle submission event

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import ExchangeService from './exchangeCall';
+
+async function getExchange(usd){
+  const response = await ExchangeService.getExchange(usd);
+  if (response.result = "success"){
+    printElements(response.conversion_rates, usd);
+  } else{
+    printError(response, usd)
+  }
+}
+
+
+function printElements(response, usd) {
+  document.getElementById('showExchange').innerText = `${usd} to ${response.conversion_rates}`
+}
+
+function printError(error, usd){
+  document.getElementById('showExchange').innerText = `${usd} conversion failed. ${error.result}`
+}
+
+function handleFormSubmission(event){
+  event.preventDefault();
+  const usd = document.getElementById('usdInput').value
+  getExchange(usd);
+
+}
+
+window.addEventListener("load", function(){
+  document.getElementById("form").addEventListener("submit", handleFormSubmission);
+});
